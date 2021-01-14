41806d04e416fff760c223b63ee2ee0f5280b952
Khuhsboobansal88 <61690410+Khuhsboobansal88@users.noreply.github.com>
2021-01-15 00:27:33 +0200

    jWT connected and token get creted with each post request

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
-const bcrypt = require("bcryptjs");
+
 //@route  POST api/users
 //@desc   Register a user
 //@access Public
@@ -44,7 +47,21 @@ router.post(
 
       await user.save();
 
-      res.send("User saved");
+      const payload = {
+        user: { id: user.id },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send("Server error");
